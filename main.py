6bf6bb82fcb26eff0376f28bd38b3156260b7981
SamroodAli <samroodali@live.com>
2020-12-12 17:10:38 +0530

    Add:money transactions function

--- main.py
+++ main.py
@@ -15,14 +15,57 @@ def coffee_options_checker(menu_data, current_resources):
     return list_options
 
 
+def money(menu_data,coffee_type):
+    total = 0
+    cost = menu_data[coffee_type]["cost"]
+    print(f"That will be ${cost}")
+    while total != cost:
+        penny = 0.01
+        nickel = 0.05
+        dime = 0.1
+        quarter = 0.25
+        try:
+            penny_input = int(input("How many pennies ?: "))
+        except:
+            penny_input = 0
+        try:
+            nickel_input = int(input("How many nickels ?: "))
+        except:
+            nickel_input = 0
+        try:
+            dime_input = int(input("How many dimes ?: "))
+        except:
+            dime_input = 0
+        try:
+            quarter_input = int(input("How many quarters ?: "))
+        except:
+            quarter_input = 0
+        total += round((penny_input*penny)+(nickel_input*nickel)+(dime_input*dime)+(quarter_input*quarter), 2)
+        if total > cost:
+            change = total - cost
+            total = cost
+            print(f"here is the balance: ${round(change, 2)}")
+        elif total < cost:
+            print(f"not enough, you are still missing ${cost-total}")
+        else:
+            return cost
+
+
 # Todo: 2: def coffee_machine => while on loop
 def coffee_machine(menu_data, current_resources):
     available_options = coffee_options_checker(menu_data, current_resources)
     if available_options:
-        print("Which coffee would you like ?")
-        for coffee_type in available_options:
-            print(coffee_type, end=" ")
-        user_choice = input()
+        user_option = ''
+        while user_option not in available_options:
+            print("Which coffee would you like ?")
+            for coffee_type in available_options:
+                print(coffee_type, end=" ")
+            user_choice = input()
+            if user_choice in available_options:
+                current_resources["money"] += money(menu_data, user_choice)
+            else:
+                print("Invalid choice")
+        current_resources = make_coffee(menu_data,current_resources)
     else:
         print("Sorry, we are out of resources")
     return
