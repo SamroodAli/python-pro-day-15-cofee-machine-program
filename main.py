8e83f54a34992fbf780e3a51a04d7cacb2ab3832
SamroodAli <samroodali@live.com>
2020-12-12 14:09:37 +0530

    Add:To-dos

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+# Todo: 1: Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+# Todo: 2: Turn off the Coffee Machine by entering “off” to the prompt.
+# Todo: 3: Print Report
+# Todo: 4: Check resources sufficient?
+# Todo: 5: Process coins
+# Todo: 6: Check transaction successful?
+# Todo: 7: Make Coffee.
+# Todo: 8: Add resources
