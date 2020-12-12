53ec8d17b73f296cb68dcf078da291fe6a0d6534
SamroodAli <samroodali@live.com>
2020-12-12 15:47:51 +0530

    Add:coffee machine function

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # Todo: 1: Import resources, screen clear
 from data import MENU, resources
 
+def coffee_options_checker(current_resources):
+    return
+
 # Todo: 2: def coffee_machine => while on loop
+def coffee_machine(input_resources):
+    available_options = coffee_options_checker(input_resources)
+    return
 # Todo: 3: check resources and show available options
 # Todo: 4: get user choice
 # Todo: 5: is off => return
