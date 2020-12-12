7e54f9ee9afbe99f28832ea72ede715dd7fa6c40
SamroodAli <samroodali@live.com>
2020-12-12 15:35:02 +0530

    Add:resource money to data file

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # Todo: 1: Import resources, screen clear
+from data import MENU, resources
+
 # Todo: 2: def coffee_machine => while on loop
 # Todo: 3: check resources and show available options
 # Todo: 4: get user choice
 # Todo: 5: is off => return
+
 # Todo: 6: is not off => def money
 # Todo: 7: Make Coffee.
 # Todo: 8: Add resources
