a75004cc9a6291a4df0fa9fa393fff61ea5ad04c
SamroodAli <samroodali@live.com>
2020-12-14 20:46:25 +0530

    Fix:changed try statements to elvis operators

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from data import MENU, resources
 from logo import LOGO
 
+
 def coffee_options_checker(menu_data, current_resources):
     list_options = []
     for coffee_type in menu_data:
@@ -15,7 +16,7 @@ def coffee_options_checker(menu_data, current_resources):
     return list_options
 
 
-def money(menu_data,coffee_type):
+def money(menu_data, coffee_type):
     total = 0
     cost = menu_data[coffee_type]["cost"]
     print(f"That will be ${cost}")
@@ -24,34 +25,29 @@ def money(menu_data,coffee_type):
         nickel = 0.05
         dime = 0.1
         quarter = 0.25
-        try:
-            penny_input = int(input("How many pennies ?: "))
-        except:
-            penny_input = 0
-        try:
-            nickel_input = int(input("How many nickels ?: "))
-        except:
-            nickel_input = 0
-        try:
-            dime_input = int(input("How many dimes ?: "))
-        except:
-            dime_input = 0
-        try:
-            quarter_input = int(input("How many quarters ?: "))
-        except:
-            quarter_input = 0
-        total += round((penny_input*penny)+(nickel_input*nickel)+(dime_input*dime)+(quarter_input*quarter), 2)
+        penny_user_input = input("How many pennies ?: ")
+        penny_input = int(penny_user_input) if penny_user_input != '' else 0
+        nickel_user_input = input("How many pennies ?: ")
+        nickel_input = int(nickel_user_input) if nickel_user_input != '' else 0
+        dime_user_input = input("How many pennies ?: ")
+        dime_input = int(dime_user_input) if dime_user_input != '' else 0
+        quarter_user_input = input("How many pennies ?: ")
+        quarter_input = int(quarter_user_input) if quarter_user_input != '' else 0
+
+        total += round(
+            (penny_input * penny) + (nickel_input * nickel) + (dime_input * dime) + (quarter_input * quarter), 2)
         if total > cost:
             change = total - cost
             total = cost
             print(f"here is the balance: ${round(change, 2)}")
             return cost
         elif total < cost:
-            print(f"not enough, you are still missing ${cost-total}")
+            print(f"not enough, you are still missing ${cost - total}")
         else:
             return cost
 
-def make_coffee(coffee_type, menu_data,current_resouces):
+
+def make_coffee(coffee_type, menu_data, current_resouces):
     print(LOGO)
     print(f"Here is your {coffee_type}, thank you for shopping, visit again.")
     ingredients = menu_data[coffee_type]["ingredients"]
@@ -84,5 +80,6 @@ def coffee_machine(menu_data, current_resources):
             on = False
             print("Sorry, we are out of resources")
     return
+
 
 coffee_machine(MENU, resources)
