a5f4c63015cd94db96141f31003d2b7723462a6e
SamroodAli <samroodali@live.com>
2020-12-15 17:07:15 +0530

    clean:coffee machine code clean, single while loop with if conditions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from logo import LOGO
 
 
 def add_resources(current_resources):
-    """"function to add resouces to current resources"""
+    """"function to add resources to current resources"""
     for resource in current_resources:
         qty = input(f"Please specify the quantity of {resource} to add : ")
         current_resources[resource] += int(qty) if qty != "" else 0
@@ -40,10 +40,9 @@ def money(menu_data, coffee_type):
             user_input = input(f"How many {coin} ?: ")
             user_input = int(user_input) if user_input != '' else 0
             total += (currency[coin]*user_input)
-        total = round(total,2)
+        total = round(total, 2)
         if total > cost:
             change = total - cost
-            total = cost
             print(f"here is the balance: ${round(change, 2)}")
             return cost
         elif total < cost:
@@ -73,32 +72,30 @@ def make_coffee(coffee_type, menu_data, current_resources):
 
 
 def coffee_machine(menu_data, current_resources):
+    """function for coffee machine"""
     is_on = True
     while is_on:
         print(LOGO)
         available_options = coffee_options_checker(menu_data, current_resources)
-        if available_options:
-            user_option = ""
-            while user_option not in available_options and user_option not in ["add", "report", "off"]:
-                print("Which coffee would you like ?")
-                for coffee_type in available_options:
-                    print(coffee_type, end=" ")
-                user_option = input().lower()
-                if user_option in available_options:
-                    current_resources["money"] += money(menu_data, user_option)
-                    current_resources = make_coffee(user_option, menu_data, current_resources)
-                elif user_option == 'off':
-                    is_on = False
-                    return
-                elif user_option == "report":
-                    reporter(current_resources)
-                elif user_option == "add":
-                    current_resources = add_resources(current_resources)
-                else:
-                    print("Invalid choice")
-        else:
-            is_on = False
+        if not available_options:
             print("Sorry, we are out of resources")
+            is_on = False
+        print("Which coffee would you like ?")
+        for coffee_type in available_options:
+            print(coffee_type, end=" ")
+        user_option = input().lower()
+        if user_option in available_options:
+            current_resources["money"] += money(menu_data, user_option)
+            current_resources = make_coffee(user_option, menu_data, current_resources)
+        elif user_option == "off":
+            is_on = False
+            return
+        elif user_option == "report":
+            reporter(current_resources)
+        elif user_option == "add":
+            current_resources = add_resources(current_resources)
+        else:
+            print("invalid choice")
     return
 
 
